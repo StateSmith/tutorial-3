360021520660531113c0c8cb5daea04494c1cdef
Adam Fraser-Kruck <adamfk@gmail.com>
2024-09-13 00:34:26 -0500

    lesson-3 - correct composition typo and improve

--- lesson-3/composition/LightSmContext.js
+++ lesson-3/composition/LightSmContext.js
@@ -2,37 +2,40 @@
 "use strict";
 
 class LightSmContext {
-    // HTML object that we will color and add text to
-    lightHtmlObject;
-
+    // public field for state machine to use
     count = 0;
 
+    // HTML object that we will color and add text to
+    // private field
+    #lightHtmlObject;
+
     // Holds milliseconds when the timer was started
-    timerStartMs = 0;
+    // private field
+    #timerStartMs = 0;
 
     constructor(lightHtmlObject) {
-        this.lightHtmlObject = lightHtmlObject;     
+        this.#lightHtmlObject = lightHtmlObject;     
     }
 
     resetTimer() {
-        this._timerStartMs = Date.now();
+        this.#timerStartMs = Date.now();
     }
 
     // Returns true if the timer was started more than 3 seconds ago
     isTimerExpired() {
-        return Date.now() - this._timerStartMs > 3000;
+        return Date.now() - this.#timerStartMs > 3000;
     }
 
     // Called by the state machine when the light should be turned on
     turnOn() {
-        this.lightHtmlObject.style.backgroundColor = "yellow";
-        this.lightHtmlObject.style.color = "black"; // text color
-        this.lightHtmlObject.textContent = "Count: " + this.count;
+        this.#lightHtmlObject.style.backgroundColor = "yellow";
+        this.#lightHtmlObject.style.color = "black"; // text color
+        this.#lightHtmlObject.textContent = "Count: " + this.count;
     }
 
     turnOff() {
-        this.lightHtmlObject.style.backgroundColor = "black";
-        this.lightHtmlObject.style.color = "white"; // text color
-        this.lightHtmlObject.textContent = "Count: " + this.count;
+        this.#lightHtmlObject.style.backgroundColor = "black";
+        this.#lightHtmlObject.style.color = "white"; // text color
+        this.#lightHtmlObject.textContent = "Count: " + this.count;
     }
 }
