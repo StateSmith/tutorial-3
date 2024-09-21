dbc38b76fb3fdd2885c5153ece0f3d81b8edc002
Adam Fraser-Kruck <adamfk@gmail.com>
2024-09-20 22:07:57 -0500

    ts wip

---
+++ lesson-4/ts-node/README.md
@@ -0,0 +1,52 @@
+# TypeScript (nodejs) Welcome!
+All the other TypeScript/JavaScript examples are web-based, so this one is a nodejs example.
+
+
+<br>
+
+# Diagram
+See [LightSm.plantuml](./LightSm.plantuml) for the PlantUML code.
+
+TODOooooooooooooooooooooooooooooooooooooooooooooooooooo
+![](docs/fsm.png)
+
+
+<br>
+
+# Run StateSmith code gen
+Run in this directory:
+```
+ss.cli run -h
+```
+
+<br>
+
+# Retrieve required packages
+Use npm to restore the required packages for handwritten user code. 
+
+**NOTE:** the generated StateSmith code does not require **any** packages.
+
+```bash
+npm install
+```
+
+<br>
+
+# Compile TypeScript to JavaScript
+Run the below command in this directory:
+```bash
+tsc
+```
+
+<br>
+
+# Run nodejs program
+```bash
+node out/index.js
+```
+
+
+<br>
+
+# More examples
+See https://github.com/StateSmith/StateSmith-examples
---
+++ lesson-4/ts-node/index.ts
@@ -0,0 +1,51 @@
+// Import the prompt-sync module
+import promptConfig from "prompt-sync";
+import * as LightSm from "./LightSm";
+
+const prompt = promptConfig({ sigint: true });
+
+
+console.log("USAGE:");
+console.log("  type i <enter> to send INCREASE event to state machine.");
+console.log("  type d <enter> to send DIM event to state machine.");
+console.log("  type o <enter> to send OFF event to state machine.");
+console.log("");
+prompt("Hit <enter> to start");
+console.log();
+
+let sm = new LightSm.LightSm();
+sm.start();
+
+while (true) {
+    ReadInputRunSm(sm);
+}
+
+function ReadInputRunSm(sm: LightSm.LightSm) {
+    console.log(`\n\nCurrent state: ${LightSm.LightSm.stateIdToString(sm.stateId)}`);
+
+    var line = prompt("Please type 'i', 'd', 'o': ");
+    let c = "";
+
+    if (line)
+        c = line[0];
+
+    RunSmForChar(sm, c);
+}
+
+function RunSmForChar(sm: LightSm.LightSm, c: string) {
+    let eventId: LightSm.EventId;
+
+    switch (c) {
+        case 'i': eventId = LightSm.EventId.INC; break;
+        case 'd': eventId = LightSm.EventId.DIM; break;
+        case 'o': eventId = LightSm.EventId.OFF; break;
+    }
+
+    if (eventId !== undefined) {
+        console.log(`Dispatching event: ${LightSm.LightSm.eventIdToString(eventId)}`);
+        sm.dispatchEvent(eventId);
+    }
+    else {
+        console.log("Invalid input. Not running state machine.");
+    }
+}
