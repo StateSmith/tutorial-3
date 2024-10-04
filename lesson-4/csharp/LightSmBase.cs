36373b9c8f241a6b415c88073c3390e388ee3f9f
Adam Fraser-Kruck <adamfk@gmail.com>
2024-10-04 10:42:32 -0500

    csharp update example

--- lesson-4/csharp/LightSmBase.cs
+++ lesson-4/csharp/LightSmBase.cs
@@ -5,9 +5,26 @@ namespace LightController;
 // user code extended by generated state machine. Nicer than expansions in most cases.
 public class LightSmBase
 {
-    public void LightBlue() => Console.WriteLine("Light is: BLUE");
-    public void LightYellow() => Console.WriteLine("Light is: YELLOW");
-    public void LightRed() => Console.WriteLine("Light is: RED");
+    public void LightBlue()
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Light is: BLUE");
+        Console.ResetColor();
+    }
+
+    public void LightYellow()
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Light is: YELLOW");
+        Console.ResetColor();
+    }
+
+    public void LightRed()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Light is: RED");
+        Console.ResetColor();
+    }
 
     protected int count;
 }
