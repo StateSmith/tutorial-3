766ed68b55f75bc5cc50e2ff2408b19dc2718616
Adam Fraser-Kruck <adamfk@gmail.com>
2024-10-04 11:06:58 -0500

    c99 add color

--- lesson-4/c99-gcc/Light.c
+++ lesson-4/c99-gcc/Light.c
@@ -1,17 +1,23 @@
 #include "Light.h"
 #include <stdio.h>
 
+// ANSI escape codes for colors
+#define RESET   "\033[0m"
+#define BLUE    "\033[0;34m"
+#define YELLOW  "\033[0;33m"
+#define RED     "\033[0;31m"
+
 void Light_blue(void)
 {
-    printf("Light is blue\n");
+    printf(BLUE "Light is blue\n" RESET);
 }
 
 void Light_yellow(void)
 {
-    printf("Light is yellow\n");
+    printf(YELLOW "Light is yellow\n" RESET);
 }
 
 void Light_red(void)
 {
-    printf("Light is red\n");
+    printf(RED "Light is red\n" RESET);
 }
